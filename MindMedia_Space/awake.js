000386b5e4a48c087971601bddb8f2e5ceb90caa
rachzl <104797624+rachzl@users.noreply.github.com>
2022-05-02 17:32:11 -0400

    Add files via upload

---
+++ MindMedia_Space/awake.js
@@ -0,0 +1,167 @@
+let camera3D, scene, renderer, cube;
+let dir = 0.01;
+let positionLimit = 40;
+let mixer;
+
+const clock = new THREE.Clock();
+
+let onPointerDownLon;
+let onPointerDownLat;
+init3D();
+
+function init3D() {
+    scene = new THREE.Scene();
+    camera3D = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1200);
+
+    renderer = new THREE.WebGLRenderer({antialias:true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+    const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+    hemiLight.position.set( 0, 200, 0 );
+    scene.add( hemiLight );
+
+    const dirLight = new THREE.DirectionalLight( 0xffffff );
+    dirLight.position.set( 0, 200, 100 );
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 180;
+    dirLight.shadow.camera.bottom = - 100;
+    dirLight.shadow.camera.left = - 120;
+    dirLight.shadow.camera.right = 120;
+    scene.add( dirLight );
+
+
+   let bgGeometery = new THREE.SphereGeometry(1000, 60, 40);
+   // let bgGeometery = new THREE.CylinderGeometry(725, 725, 1000, 10, 10, true)
+    bgGeometery.scale(-1, 1, 1);
+    // has to be power of 2 like (4096 x 2048) or(8192x4096).  i think it goes upside down because texture is not right size
+    let panotexture = new THREE.TextureLoader().load("land.jpg");
+    // var material = new THREE.MeshBasicMaterial({ map: panotexture, transparent: true,   alphaTest: 0.02,opacity: 0.3});
+    let backMaterial = new THREE.MeshBasicMaterial({ map: panotexture });
+
+    let back = new THREE.Mesh(bgGeometery, backMaterial);
+    scene.add(back);
+
+    moveCameraWithMouse();
+
+    camera3D.position.z = 5;
+
+    const fbxLoader = new THREE.FBXLoader()
+    
+    fbxLoader.load(
+        'dance.fbx',
+        (object) => {
+
+
+            mixer = new THREE.AnimationMixer( object );
+
+            const action = mixer.clipAction( object.animations[ 0 ] );
+            action.play();
+
+            object.traverse( function ( child ) {
+
+                if ( child.isMesh ) {
+
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+
+                }
+
+            } );
+
+            object.scale.set(0.02,0.02,0.02);
+
+            scene.add(object)
+        },
+
+    )
+
+
+
+
+    animate();
+}
+
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    const delta = clock.getDelta();
+    if ( mixer ) mixer.update( delta );
+    renderer.render(scene, camera3D);
+}
+
+
+
+/////MOUSE STUFF
+
+var onMouseDownMouseX = 0, onMouseDownMouseY = 0;
+var onPointerDownPointerX = 0, onPointerDownPointerY = 0;
+var lon = -90, onMouseDownLon = 0;
+var lat = 0, onMouseDownLat = 0;
+var isUserInteracting = false;
+
+
+function moveCameraWithMouse() {
+    document.addEventListener('keydown', onDocumentKeyDown, false);
+    document.addEventListener('mousedown', onDocumentMouseDown, false);
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
+    document.addEventListener('mouseup', onDocumentMouseUp, false);
+    document.addEventListener('wheel', onDocumentMouseWheel, false);
+    window.addEventListener('resize', onWindowResize, false);
+    camera3D.target = new THREE.Vector3(0, 0, 0);
+}
+
+function onDocumentKeyDown(event) {
+    //if (event.key == " ") {
+    //in case you want to track key presses
+    //}
+}
+
+function onDocumentMouseDown(event) {
+    onPointerDownPointerX = event.clientX;
+    onPointerDownPointerY = event.clientY;
+    onPointerDownLon = lon;
+    onPointerDownLat = lat;
+    isUserInteracting = true;
+}
+
+function onDocumentMouseMove(event) {
+    if (isUserInteracting) {
+        lon = (onPointerDownPointerX - event.clientX) * 0.1 + onPointerDownLon;
+        lat = (event.clientY - onPointerDownPointerY) * 0.1 + onPointerDownLat;
+        computeCameraOrientation();
+    }
+}
+
+function onDocumentMouseUp(event) {
+    isUserInteracting = false;
+}
+
+function onDocumentMouseWheel(event) {
+    camera3D.fov += event.deltaY * 0.05;
+    camera3D.updateProjectionMatrix();
+}
+
+function computeCameraOrientation() {
+    lat = Math.max(- 30, Math.min(30, lat));  //restrict movement
+    let phi = THREE.Math.degToRad(90 - lat);  //restrict movement
+    let theta = THREE.Math.degToRad(lon);
+    camera3D.target.x = 100 * Math.sin(phi) * Math.cos(theta);
+    camera3D.target.y = 100 * Math.cos(phi);
+    camera3D.target.z = 100 * Math.sin(phi) * Math.sin(theta);
+    camera3D.lookAt(camera3D.target);
+}
+
+
+function onWindowResize() {
+    camera3D.aspect = window.innerWidth / window.innerHeight;
+    camera3D.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    console.log('Resized');
+}
+
+
